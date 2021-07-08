9ad86bf28ef92bbcddb15c0ad0cd4b5aca76e9be
Sebastian Vargas <vargaspizango1@gmail.com>
2021-07-08 18:42:15 -0500

    feat: corrections added and moongoose & cors too

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
 
 //Crear el servidor de express
 const app = express();
 
-// bVvOa5kqtgFbY25u
-// mean_user
+//Configuración del CORS
+app.use(cors());
+
+// Base de datos
+dbConnection();
 
 // Rutas
 app.get('/', (req, res) => {
@@ -14,8 +20,7 @@ app.get('/', (req, res) => {
     })
 });
 
-
 // Montar el servidor de express
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
-})
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
+});
