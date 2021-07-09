0f5062f6ab890de9843e53cc39b56e1a18bbc049
Sebastian Vargas <vargaspizango1@gmail.com>
2021-07-08 20:01:11 -0500

    minor: GET users improved
    The route returns now an uid property instead _id

--- models/usuario.js
+++ models/usuario.js
@@ -28,5 +28,12 @@ const UsuarioSchema = Schema({
   },
 });
 
+UsuarioSchema.method('toJSON', function () {
+  const { __v, _id, ...object } = this.toObject();
+
+  object.uid = _id
+  return object;
+})
+
 // Por defecto, Mongoose pone en plural
 module.exports = model('Usuario', UsuarioSchema);
