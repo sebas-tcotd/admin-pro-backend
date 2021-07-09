e1800cc363bf5271ffd5892d5766eb0365de147d
Sebastian Vargas <vargaspizango1@gmail.com>
2021-07-08 20:47:45 -0500

    feat: validations improved

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,11 +3,20 @@
  */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { getUsuarios, crearUsuario } = require('../controllers/usuarios');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 router.get('/', getUsuarios);
-router.post('/', crearUsuario)
+router.post('/',
+  [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'La contraseña es obligatoria').not().isEmpty(),
+    check('email', 'El correo es obligatorio').not().isEmpty(),
+    validarCampos,
+  ],
+  crearUsuario);
 
 module.exports = router;
