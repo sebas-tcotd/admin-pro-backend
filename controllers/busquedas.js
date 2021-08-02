2cf54cb195d3f8a3fe89143bb6f9f356d2d6f4b2
Sebastian Vargas <vargaspizango1@gmail.com>
2021-08-01 19:53:45 -0500

    feat: specific search by tables implemented

--- controllers/busquedas.js
+++ controllers/busquedas.js
@@ -23,6 +23,45 @@ const getTodo = async (req, res = response) => {
   });
 }
 
+const getDocumentosColeccion = async (req, res = response) => {
+  const table = req.params.table;
+  const term = req.params.term;
+  const regex = new RegExp(term, 'i');
+
+  let data = [];
+
+  switch (table) {
+    case 'medicos':
+      data = await Medico.find({ name: regex })
+        .populate('usuario', 'name img')
+        .populate('hospital', 'name img');
+      break;
+
+    case 'usuarios':
+      data = await Usuario.find({ name: regex })
+      break;
+
+    case 'hospitales':
+      data = await Hospital.find({ name: regex })
+        .populate('usuario', 'name img');
+      break;
+
+    default:
+      return res.status(400).json({
+        ok: false,
+        msg: "La tabla tiene que ser 'usuarios', 'medicos' u 'hospitales'."
+      });
+
+
+  }
+
+  res.json({
+    ok: true,
+    resultados: data,
+  });
+}
+
 module.exports = {
-  getTodo
+  getTodo,
+  getDocumentosColeccion,
 }
--- routes/busquedas.js
+++ routes/busquedas.js
@@ -2,11 +2,13 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { getTodo } = require('../controllers/busquedas');
+const { getTodo, getDocumentosColeccion } = require('../controllers/busquedas');
 const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
 router.get('/:term', [ validarJWT ], getTodo);
+
+router.get('/colection/:table/:term', validarJWT, getDocumentosColeccion);
 
 module.exports = router;
