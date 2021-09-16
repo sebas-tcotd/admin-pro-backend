5822fc13540af64687d150dd21870c0ba5f7e808
Sebastian Vargas <vargaspizango1@gmail.com>
2021-09-16 00:29:59 -0500

    ci: add SPA control

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
@@ -25,6 +26,11 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/login', require('./routes/auth'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/upload', require('./routes/uploads'));
+
+// Fallback
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+})
 
 // Montar el servidor de express
 app.listen(process.env.PORT, () => {
